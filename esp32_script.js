b3a63a1e6ab49fcfc5bca4a78d002e3aa5845315
simonyipeter <simonyip001@gmail.com>
2017-08-27 15:19:42 +0200

    Update esp32_script.js

--- esp32_script.js
+++ esp32_script.js
@@ -1,22 +1,17 @@
-var refresh_time = 5;
-
-setTimeout(function(){
-	   window.location.reload(1);
-	}, refresh_time*1000);
+/
 
 $( document ).ready(function() {
     //console.log( "ready!" );
 	
 	// Instantiate a slider
-	$('#csuszka').slider({
-						formatter: function(value) { return 'Current value: ' + value;			}
-							})	
-	$("#csuszka").on("slide", function(slideEvt) {
-						refresh_time=slideEvt.value;
-						console.log( refresh_time );
-					});
+	$('#csuszka').slider({				});	
+	$("#csuszka").on("slide", function(slideEvt) {						$("#slider_val").text(slideEvt.value);					});
 		
 	$('.row').css('margin-bottom','50px');
+	
+	setTimeout(function(){
+	   window.location.reload(1);
+	}, $("#ref_time").val()*1000);
 	
 	
 });
@@ -27,21 +22,38 @@ $( document ).ready(function() {
 
 function button_one(){ 
 		if($( "#button_one" ).hasClass( "btn-success" )){ 
-					$.ajax({  url: "/gpio-5/",
+					$.ajax({  url: "/gpio/5/off",
 					   success: function(data){
 							if(data=="OK"){	$('#button_one').removeClass("btn-success"); $('#button_one').addClass("btn-danger");	}
-					   },
-					   timeout: 1000 //in milliseconds
+					   },  timeout: 1000 //in milliseconds
 					});
 				}
 		else if($( "#button_one" ).hasClass( "btn-danger" )){ 
 				
-				
-					$.ajax({  url: "/gpio-5/",
+					$.ajax({  url: "/gpio/5/on",
 					   success: function(data){
 							if(data=="OK"){	$('#button_one').removeClass("btn-danger"); $('#button_one').addClass("btn-success");	}
-					   },
-					   timeout: 1000 //in milliseconds
+					   },   timeout: 1000 //in milliseconds
 					});
 				}
+	}
+	
+function ref_time(){ 
+		
+					$.ajax({  url: "/ref_time/"+$("#ref_time").val()+"",
+					   success: function(data){
+							if(data=="OK"){		window.location.reload(1); }
+					   },  timeout: 1000 //in milliseconds
+					});
+				
+	}
+	
+function button_demo_1(){ 
+		
+					$.ajax({  url: "/demo_1/"+$("#csuszka").val()+"",
+					   success: function(data){
+							if(data=="OK"){		window.location.reload(1); }
+					   },  timeout: 1000 //in milliseconds
+					});
+				
 	}
